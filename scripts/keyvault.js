3695c12ec59115b745f7c2741df79d3d1df2b978
abidfs <abidfs@gmail.com>
2023-08-02 17:56:46 +0100

    chore: updated env var name from KEYVAULT_NAME to VAULT_NAME

--- scripts/keyvault.js
+++ scripts/keyvault.js
@@ -3,16 +3,19 @@ const { DefaultAzureCredential } = require("@azure/identity");
 
 const loadSecrets = async () => {
   try {
-    const keyvaultUrl = `https://${process.env.KEYVAULT_NAME}.vault.azure.net`;
+    const keyvaultUrl = `https://${process.env.VAULT_NAME}.vault.azure.net`;
     if (keyvaultUrl) {
       const credential = new DefaultAzureCredential();
+      console.log('credentials created');
       const client = new SecretClient(keyvaultUrl, credential);
+      console.log("SecretClient created");
 
       for await (const secret of client.listPropertiesOfSecrets()) {
         if (secret.enabled) {
           const key = secret.name;
+          console.log("loading secret ", key);
           const value = await client.getSecret(key);
-          console.log(".................", key, value);
+          console.log("secret value", value);
         }
       }
     }
